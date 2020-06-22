f22b66b40e45ce9b02122b46836f60af84ab0b0f
joelhfuenmayor <67279515+joelhfuenmayor@users.noreply.github.com>
2020-06-22 11:04:42 -0400

    Add files via upload

---
+++ procesa.php
@@ -0,0 +1,11 @@
+<?php
+ 
+$base = $_GET["cbase"];
+ 
+$altura = $_GET["caltura"];
+ 
+$Hipo = sqrt(pow($base,2) + pow($altura,2));
+ 
+echo "Valor de Hipotenusa del Triangulo Rectangulo es: " . $Hipo;echo" Cm";
+ 
+?>
